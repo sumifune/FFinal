2581923411b1e0e99663e5cce5a55a3f14e49f26
sumifune <sukia.mifune@gmail.com>
2021-01-03 23:10:59 +0100

    first commit

---
+++ precache-manifest.da1fccde8e32d553e5b7d72c47831e5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "967734b4f46dd4fc4fe5e10e3ef0d383",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afda724b4c3becf03026",
+    "url": "/static/css/2.4df217b3.chunk.css"
+  },
+  {
+    "revision": "c23446cdae5daf8e5399",
+    "url": "/static/css/main.ee186c3d.chunk.css"
+  },
+  {
+    "revision": "afda724b4c3becf03026",
+    "url": "/static/js/2.47746b2d.chunk.js"
+  },
+  {
+    "revision": "10cc33dab85f021f8a3281f13ee4531b",
+    "url": "/static/js/2.47746b2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c23446cdae5daf8e5399",
+    "url": "/static/js/main.4f88d863.chunk.js"
+  },
+  {
+    "revision": "5820d58c60ab3b1375ad",
+    "url": "/static/js/runtime-main.99b514f4.js"
+  }
+]);
